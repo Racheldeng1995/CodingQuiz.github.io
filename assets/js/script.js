b41c9fc6c4e49c5cbcf19e1ebdb7aedfa74d9f35
Racheldeng1995 <racheldeng1995@gmail.com>
2022-07-05 18:10:44 -0700

    add quiz content and styling on css

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ var buttonEl =document.querySelector("#start-btn")
 
 var startCountDown = function() {
     var timeLeft = 100;
-    
+
     setInterval(function () {
         timeLeft--;
         if (timeLeft === 0) {
@@ -18,5 +18,10 @@ var startCountDown = function() {
 
 buttonEl.addEventListener("click",function(){
     startCountDown();
+    startQuiz();
 })
 
+var startQuiz = function showDiv() {
+    document.getElementById('content-quiz').style.display = "block";
+    document.getElementById('content-start').style.display = "none";
+ }
