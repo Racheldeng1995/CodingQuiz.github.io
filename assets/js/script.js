554a24f818bc32909b71d6b8ccf432b5e178128e
Racheldeng1995 <racheldeng1995@gmail.com>
2022-07-07 12:40:58 -0700

    add comment on js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//question bank
 var quizQustion = [
     {
         quizId : 1,
@@ -40,16 +41,16 @@ var quizQustion = [
     }
 ]
 
+//get main section from html
 var timerEl = document.querySelector("#timer")
 var buttonEl = document.querySelector("#start-btn")
 var quizEl = document.querySelector("#content-quiz")
 var allDoneSec = document.querySelector("#all-done")
 var finalPageSec = document.querySelector("#final-page")
-console.log(finalPageSec)
 
 
+//start count down function, can inout any value as start counting point
 var timeInterval = null
-
 var startCountDown = function(num) {
     var timeLeft = num;
 
@@ -65,28 +66,29 @@ var startCountDown = function(num) {
     }, 1000)
 }
 
+//stop count down function, to stop the current timer
 function stopCountDown() {
-    // To cancel an interval, pass the timer to clearInterval()
     clearInterval(timeInterval);
   }
 
+
+//check answer function
+//it's used to check if the user give the correct answer
+//and create a section on html to display the result
 var checkAnswer = function(answer, input){
     var ansEl = document.createElement("p")
     ansEl.className = "quiz-anwser"
     if (input == answer-1) {
         ansEl.innerHTML = "Correct!"
-        console.log(input)
-        console.log(answer)
     }
     else {
         ansEl.innerHTML = "Wrong!"
-        console.log(input)
-        console.log(answer)
     }
 
     return ansEl
 }
 
+//This function is used to create html and css styling for all done section
 var allDone = function(time) {
     document.getElementById('content-quiz').style.display = "none";
     document.getElementById('all-done').style.display = "block";
@@ -111,19 +113,18 @@ var allDone = function(time) {
     allDoneSec.appendChild(allDoneEl)
     allDoneSec.appendChild(scoreEl)
     allDoneSec.appendChild(initialEl)
-    console.log(allDoneSec)
 }
 
 
+//This function is used to create html and css styling for final saving page section
+//The input is getting from the local storage value
 getHighScore = function (result) {
     document.getElementById('all-done').style.display = "none";
     document.getElementById('final-page').style.display = "block";
     var finalPageEl = document.createElement("h1")
     finalPageEl.innerHTML = "High scores"
-    console.log(finalPageEl)
     var scoreEl = document.createElement("p")
     scoreEl.innerHTML = "1. " + result
-    console.log(scoreEl)
     var finalButtonEl = document.createElement("section")
     finalButtonEl.className = "form"
     var goBackBtn = document.createElement("button")
@@ -135,49 +136,42 @@ getHighScore = function (result) {
     clearHighScore.className = "form-submit"
     finalButtonEl.appendChild(goBackBtn)
     finalButtonEl.appendChild(clearHighScore)
-    console.log(finalButtonEl)
     finalPageSec.appendChild(finalPageEl)
     finalPageSec.appendChild(scoreEl)
     finalPageSec.appendChild(finalButtonEl)
-    console.log(finalPageSec)
-    //alert("here")
 }
 
-var penaltyCountDown = function() {
 
-}
-
+// start the quiz test with clicking onthe start button
 buttonEl.addEventListener("click",function(){
     startQuiz();
 })
 
-// if (localStorage.getItem("quizResult") == null) {
-//     var quizResult = []
-//     getResult();
-// }
-// else if (localStorage.getItem("quizResult") != null) {
-//     var quizResult = JSON.parse(localStorage.getItem("quizResult"))
-//     getResult()
-// }
 
 var quizCounter = 0
 
-// when any of the button of the quiz gets clicked, create next question
+// The function that execute the algorithm and listen the user behavior
 var startQuiz = function () {
+    //start count down
     startCountDown(100);
     document.getElementById('content-start').style.display = "none";
 
-
+    //when any of the button of the quiz gets clicked, create next question
     var createQuizEl = function(quizCounter) {
         document.getElementById('content-quiz').style.display = "block";
+
+        //get the correct answer for later comparison
         var ansValue = quizQustion[quizCounter]["quizAnswer"]
-        console.log(ansValue)
+        
+        //create question 
         quizEl.innerHTML = 
             "<h1>" + quizQustion[quizCounter]["quizDetails"]["quizQuestion"] + "</h1>";
-        console.log(quizEl)
+        
+        
+        //create question choice
         var quizChoiceEl = document.createElement("ul");
-        console.log(quizChoiceEl)
-        var listBtnEl
+        
+        //create question button
         for(var j = 0; j < quizQustion[quizCounter]["quizDetails"]["quizChoice"].length; j++ ) {
             (function (j) {
             var listQuizEl = document.createElement("li")
@@ -185,20 +179,23 @@ var startQuiz = function () {
             listBtnEl.className = "main-quiz-btn"
             listBtnEl.id = "but"+"" + quizQustion[quizCounter]["quizId"] + j
             listBtnEl.innerHTML = quizQustion[quizCounter]["quizDetails"]["quizChoice"][j]
-            console.log(listBtnEl)
-            var inputValue = listBtnEl.id
-            console.log(inputValue)
 
+            var inputValue = listBtnEl.id
+            
+            //listen user behaviors
             listBtnEl.addEventListener("click", function() {
                 
+                //if the question is not the last one, then click button, go to the next question
+                //if the question is the last one, click button go to the game over page
+                //if run out of time, click any button, go to the game over page
                 if (quizCounter < 2 && timerEl.getAttribute("value") != 0) {
                     createQuizEl(quizCounter+1);
                     quizEl.append(checkAnswer(ansValue,inputValue.slice(inputValue.length - 1)));
                     var ans = checkAnswer(ansValue,inputValue.slice(inputValue.length - 1))
                     if(ans.innerHTML == "Wrong!") {
-                        //alert("here")
+
+                        //if the user clicks the wrong answer, subtract 10 sec from timer
                         var remain = timerEl.getAttribute("value") - 10
-                        console.log(remain)
                         stopCountDown()
                         startCountDown(remain)
                         
@@ -207,24 +204,21 @@ var startQuiz = function () {
                 else{
                     var ans = checkAnswer(ansValue,inputValue.slice(inputValue.length - 1))
                     if(ans.innerHTML == "Wrong!") {
-                        
                         var remain = timerEl.getAttribute("value") - 10
-                        console.log(remain)
                         stopCountDown()
                         startCountDown(remain)
                     }
                     else{
                         var remain = timerEl.getAttribute("value")
                     }
+
                     allDone(remain)
-                    console.log(remain)
                     allDoneSec.appendChild(checkAnswer(ansValue,inputValue.slice(inputValue.length - 1)))
                     stopCountDown()
-                    var submitBtn = document.getElementById ("submit-btn")
-                    console.log(submitBtn)
 
+                    //submit button functionality
+                    //set and get the value from local storage
                     var submitBtn = document.getElementById ("submit-btn")
-                    console.log(submitBtn)
 
                     if (localStorage.getItem("quizResult") == null) {
                         var quizResult = []
@@ -235,191 +229,44 @@ var startQuiz = function () {
 
                     submitBtn.addEventListener("click", function(event) {
                         event.preventDefault();
+
                         var quizResultName = document.getElementById("ini-name").value
                         var quizResultScore = remain
-                        console.log(quizResultName)
-                        console.log(quizResultScore)
-                        resultArray = {"name": quizResultName, "score":quizResultScore}
-                        console.log(resultArray)
+                        var resultArray = {"name": quizResultName, "score":quizResultScore}
                         quizResult.push(resultArray)
-                        console.log(quizResult)
-                        localStorage.setItem("quizResult", JSON.stringify(quizResult));
-                        var finalScore = JSON.parse(localStorage.getItem("quizResult"))
-                        console.log(finalScore)
                         
+                        localStorage.setItem("quizResult", JSON.stringify(quizResult))
+
+                        var finalScore = JSON.parse(localStorage.getItem("quizResult"))                      
                         var highest = Math.max(...finalScore.map(o => o.score))
-                        console.log(highest)
-                        var highestItem = finalScore.filter(item => item.score == highest)
-                        console.log(highestItem)
+                        var highestItem = finalScore.filter(item => item.score == highest)    
                         var finalResult = highestItem[0]["name"]+"-"+highestItem[0]["score"]
-                        console.log(finalResult)
+    
                         getHighScore(finalResult)  
+
+                        //go back to main page button
                         var goBack = document.getElementById ("go-back")
-                        console.log(document.getElementById ("go-back"))
+
                         goBack.addEventListener("click", function(event) {
                             event.preventDefault();
                             document.getElementById('content-start').style.display = "block";
                             document.getElementById('final-page').style.display = "none";
                         })                      
-                        })
+                    })
 
 
                 }
             })
-            console.log(listBtnEl)
-            listQuizEl.appendChild(listBtnEl)
-            console.log(listQuizEl)
+            
+            listQuizEl.appendChild(listBtnEl)           
             quizChoiceEl.appendChild(listQuizEl)
-            console.log(quizChoiceEl)
             }(j))
         }
         quizEl.appendChild(quizChoiceEl)
-        console.log(quizEl)
-
-        btnArray = []
-        
+    
     }
 
     createQuizEl(0)
-
-// var submitBtn = document.getElementById ("submit-btn")
-// console.log(submitBtn)
-
-// if (localStorage.getItem("quizResult") == null) {
-//     var quizResult = []
-// }
-// else if (localStorage.getItem("quizResult") != null) {
-//     var quizResult = JSON.parse(localStorage.getItem("quizResult"))
-// }
-
-// submitBtn.addEventListener("click", function() {
-//     var quizResultName = document.getElementById("ini-name").value
-//     var quizResultScore = timerEl.getAttribute("value")
-//     console.log(quizResultName)
-//     console.log(quizResultScore)
-//     resultArray = {"name": quizResultName, "score":quizResultScore}
-//     console.log(resultArray)
-//     quizResult.push(resultArray)
-//     console.log(quizResult)
-//     localStorage.setItem("quizResult", JSON.stringify(quizResult));
-//     var finalScore = JSON.parse(localStorage.getItem("quizResult"))
-//     console.log(finalScore)
-    
-//     var highest = Math.max(...finalScore.map(o => o.score))
-//     console.log(highest)
-//     var highestItem = finalScore.filter(item => item.score == highest)
-//     console.log(highestItem)
-//     var finalResult = highestItem[0]["name"]+"-"+highestItem[0]["score"]
-//     console.log(finalResult)
-//     getHighScore(finalResult)
-//     alert("here")
-//     })
-
-// var saveResult = function() {
-//     var submitBtn = document.getElementById ("submit-btn")
-//     console.log(submitBtn)
-  
-//     var getResult = function() {submitBtn.addEventListener("click", function() {
-//         var quizResultName = document.getElementById("ini-name").value
-//         var quizResultScore = timerEl.getAttribute("value")
-//         console.log(quizResultName)
-//         console.log(quizResultScore)
-//         resultArray = {"name": quizResultName, "score":quizResultScore}
-//         console.log(resultArray)
-//         quizResult.push(resultArray)
-//         console.log(quizResult)
-//         localStorage.setItem("quizResult", JSON.stringify(quizResult));
-//         var finalScore = JSON.parse(localStorage.getItem("quizResult"))
-//         console.log(finalScore)
-        
-//         var highest = Math.max(...finalScore.map(o => o.score))
-//         console.log(highest)
-//         var highestItem = finalScore.filter(item => item.score == highest)
-//         console.log(highestItem)
-//         var finalResult = highestItem[0]["name"]+"-"+highestItem[0]["score"]
-//         console.log(finalResult)
-//         getHighScore(finalResult)
-//         alert("here")
-//         })
-//     }
-//     if (localStorage.getItem("quizResult") == null) {
-//         var quizResult = []
-//         getResult();
-//     }
-//     else if (localStorage.getItem("quizResult") != null) {
-//         var quizResult = JSON.parse(localStorage.getItem("quizResult"))
-//         getResult()
-//     }
-// };
-
-    // if (quizCounter < 3) {
-    //     createQuizEl(quizCounter)
-    //     console.log(quizCounter)
-        
-    // }
-    // else {
-    //     allDone(timerEl.getAttribute("value"))
-    // }
-
-    // quizCounter++
-    // var btnEl= []
-    // for (i = 0; i < iniQuestion.length; 1++) {
-    //     var inputValue = iniQuestion[i]
-    //     btnEl[i] = document.getElementById(inputValue)
-    //     console.log(inputValue)
-    //     console.log(btnEl[i]); 
-    //     btnEl[i].addEventListener('click', function (event) {
-    //         event.preventDefault();  
-    //         createQuizEl(quizQustion[counter]["quizId"], quizQustion[counter]["quizDetails"]["quizQuestion"], quizQustion[counter]["quizDetails"]["quizChoice"])
-    //         console.log(ansValue)
-    //         console.log(inputValue)
-    //         console.log(inputValue.slice(inputValue.length - 1))
-    //         checkAnswer(ansValue,inputValue.slice(inputValue.length - 1))
-    //         },false)
-
-    // }
-    // var btnItem = []
-    // //For each question on the bank..
-    // //for ( i = 0 ; i < quizQustion.length; i++) {
-    // for ( i = 0 ; i < 2 ; i++) {
-    //     btnItem[i] = createQuizEl(quizQustion[i]["quizId"], quizQustion[i]["quizDetails"]["quizQuestion"], quizQustion[i]["quizDetails"]["quizChoice"])
-    //     console.log('here')
-    //     console.log(btnItem[i])
-    //     console.log(btnItem[i][0])
-    //     console.log(quizEl)
-    //     console.log(document.getElementById(btnItem[i][0]))
-    //     console.log(quizQustion[i]["quizAnswer"])
-    //     var ansValue = quizQustion[i]["quizAnswer"]
-
-    //     var btnEl= []
-    //     //For each button?
-    //     for(let m = 0; m < btnItem[i].length; m++) {
-    //         var inputValue = btnItem[i][m]
-    //         btnEl[m] = document.getElementById(inputValue)
-    //         console.log(inputValue)
-    //         console.log(btnEl[m]); 
-    //         btnEl[m].addEventListener('click', function (event) {
-    //             event.preventDefault();  
-    //             console.log(ansValue)
-    //             console.log(inputValue)
-    //             console.log(inputValue.slice(inputValue.length - 1))
-    //             checkAnswer(ansValue,inputValue.slice(inputValue.length - 1))
-    //             },false)   
-    //         }
-
-    //     // btnEl[m].addEventListener('click', function (event) {
-    //     //     console.log('clicked');
-    //     // }, false)
-    
-    //     }
-    // const btnQuiz = document.querySelectorAll('button[id^=but]')
-    // btnQuiz.forEach(btn => {
-    // btn.addEventListener('click', event => {
-    //         console.log( event.target.id );
-
-    //     });
-    // });
-    
 }
 
 
